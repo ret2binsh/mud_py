80a31dc007d74c208f44f1663aadb96f7be0543b
Joshua Gonzales <joshua.b.gonzales@gmail.com>
2018-10-22 14:14:09 -0500

    Moved item instantiation into the items.py file and creating the random item generator for the rooms. Also, cleaned up the item class to only require a name and description and will then use the name to generate the displayName using the ANSI escape codes.

--- data/items.py
+++ data/items.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+# ANSI escape codes for changing the terminal colors when sending messages
+color = {
+    "black": u"\u001b[30;1m",
+    "red": u"\u001b[31;1m",
+    "green": u"\u001b[32;1m",
+    "yellow": u"\u001b[33;1m",
+    "blue": u"\u001b[34;1m",
+    "magenta": u"\u001b[35;1m",
+    "cyan": u"\u001b[36;1m",
+    "white": u"\u001b[37;1m",
+    "reset": u"\u001b[0m"
+    }
+
 class Generic_Items(object):
     """
     Create the base class for all items. This will mainly handle the most
@@ -31,11 +48,11 @@ class Stationary_Items(object):
 
 class Drink(PickUp_Items):
 
-    def __init__(self,displayName,name,description):
+    def __init__(self,name,description):
     # Initiate Drink item attributes
         super(Drink,self).__init__()
-        self.displayName = displayName
         self.name = name
+        self.displayName = u"%s%s%s" % (color["white"],name,color["reset"])
         self.description = description
         self.consume = True
         self.equip = False
@@ -43,12 +60,97 @@ class Drink(PickUp_Items):
 
 class Weapon(PickUp_Items):
 
-    def __init__(self,displayName,name,description,power):
+    def __init__(self,name,description,power):
     # Initiate Weapon item attributes
         super(Weapon,self).__init__()
-        self.displayName = displayName
         self.name = name
+        self.displayName = u"%s%s%s" % (color["red"],name,color["reset"])
         self.description = description
         self.consume = False
         self.power = 10 * power
         self.equip = "weapon"
+
+class Armor(PickUp_Items):
+
+    def __init__(self,name,description,defense):
+    # Initiate Weapon item attributes
+        super(Armor,self).__init__()
+        self.name = name
+        self.displayName = u"%s%s%s" % (color["cyan"],name,color["reset"])
+        self.description = description
+        self.consume = False
+        self.defense = 2 * defense
+        self.equip = "armor"
+
+
+tier1Weapons = [
+    Weapon(
+        "Dagger",
+        "A rusty dagger.",
+        1
+        ),
+    Weapon(
+        "Broad Sword",
+        "A boring sword.",
+        1
+        ),
+    Weapon(
+        "Javelin",
+        "A pokey weapon.",
+        1
+        ),
+    Weapon(
+        "Pistol",
+        "A crummy pistol.",
+        1
+        )
+    ]
+
+tier1Armor = [
+    Armor(
+        "Chain Mail",
+        "A heavy set of chain mail.",
+        1
+        ),
+    Armor(
+        "Leather Hide",
+        "Crude garments made from leather.",
+        1
+        ),
+    Armor(
+        "Black Tights",
+        "Rather tight looking.",
+        1
+        )
+    ]
+
+tier1Drinks = [
+    Drink(
+        "Beer",
+        "Warm beer"
+        ),
+    Drink(
+        "Wine",
+        "Dank wine"
+        ),
+    Drink(
+        "Orange Juice",
+        "Spoiled orange juice."
+        )
+    ]
+
+tier1Items = [tier1Armor,tier1Drinks,tier1Weapons]
+
+def random_items():
+    """ Function that seeds the room with a random quantity and type of
+    either drink, weapon, or armor items.
+    """
+
+    itemsList = []  #list of items to be returned
+
+    for i in range(0,random.choice([1,1,1,2])):
+        # randomly determine number of items and select item from lists
+        type = random.choice(tier1Items)
+        itemsList.append(random.choice(type))
+
+    return itemsList
--- data/rooms.py
+++ data/rooms.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
-
 import items
 
-# ANSI escape codes for changing the terminal colors when sending messages
-color = {
-    "black": u"\u001b[30;1m",
-    "red": u"\u001b[31;1m",
-    "green": u"\u001b[32;1m",
-    "yellow": u"\u001b[33;1m",
-    "blue": u"\u001b[34;1m",
-    "magenta": u"\u001b[35;1m",
-    "cyan": u"\u001b[36;1m",
-    "white": u"\u001b[37;1m",
-    "reset": u"\u001b[0m"
-    }
+class Rooms(object):
+    """ Parent class that holds all of the methods that each child room
+    class will inherit.
+    """
+
+    def enter_room(self,choice):
+        # returns a room object using the roomExits dictionary
+
+        return roomExits[choice]
 
 class PhoenixTavern(object):
     """ Room class object that initializes with a name, long longDescription,
@@ -23,23 +18,12 @@ class PhoenixTavern(object):
     """
 
     def __init__(self):
+        super(PhoenixTavern,self).__init__()
         self.name = "Phoenix Tavern"
         self.longDescription = ("You're in a dusty tavern in Phoenix, Az. "
             "There are a few patrons but no one seems to notice you.")
         self.description = "You are in the Phoenix Tavern."
-        self.items = [
-            items.Drink(
-                u"%sBeer%s" % (color["white"],color["reset"]),
-                "Beer",
-                "A delicious looking beer!"
-                ),
-            items.Weapon(
-                u"%sDagger%s" % (color["red"],color["reset"]),
-                "Dagger",
-                "A rusty dagger.",
-                1
-                )
-            ]
+        self.items = items.random_items()
         self.exits = ["Phoenix Train Station"]
 
 class PhoenixTrainStation(object):
@@ -50,18 +34,12 @@ class PhoenixTrainStation(object):
     """
 
     def __init__(self):
+        super(PhoenixTrainStation,self).__init__()
         self.name = "Phoenix Train Station"
         self.longDescription = ("The Phoenix train station. From here you can "
             "travel to Flagstaff, Kingman, Tucson, or Yuma.")
         self.description = "This is the Phoenix Train Station."
-        self.items = [
-            items.Weapon(
-                u"%sBuster Sword%s" % (color["red"],color["reset"]),
-                "Buster Sword",
-                "savage looking buster sword.",
-                10
-                )
-            ]
+        self.items = items.random_items()
         self.exits = ["Phoenix Tavern"]
 
 # used in gamefunctions for matching the enter command parameter with an
